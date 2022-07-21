0d41f34caed859352d30788863d2a72f86dd2b94
swapnil7000 <swapnil.bhojwani789@gmail.com>
2022-07-21 23:49:52 +0530

    Loaded images from coil library

--- app/src/main/java/com/swapnil/profilecard/MainActivity.kt
+++ app/src/main/java/com/swapnil/profilecard/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -17,10 +16,12 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
 import com.swapnil.profilecard.ui.theme.LightGreen
 import com.swapnil.profilecard.ui.theme.ProfileCardTheme
 
@@ -63,7 +64,7 @@ fun ProfileCard(userProfile: UserProfile) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
-            ProfilePicture(userProfile.drawableId, userProfile.isActive)
+            ProfilePicture(userProfile.isActive, userProfile.imageUrl)
             ProfileContent(userProfile.userName, userProfile.isActive)
         }
     }
@@ -92,7 +93,7 @@ fun ProfileContent(userName: String, active: Boolean) {
 }
 
 @Composable
-fun ProfilePicture(drawableId: Int, isActive: Boolean) {
+fun ProfilePicture(isActive: Boolean, imageUrl: String) {
     Card(
         shape = CircleShape, border = BorderStroke(
             2.dp,
@@ -102,11 +103,12 @@ fun ProfilePicture(drawableId: Int, isActive: Boolean) {
                 Color.Red
         )
     ) {
-        Image(
-            painter = painterResource(id = drawableId),
-            contentDescription = "content",
-            Modifier.size(72.dp),
-            contentScale = ContentScale.FillHeight
+        SubcomposeAsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(imageUrl)
+                .transformations(CircleCropTransformation()).build(),
+            loading = { CircularProgressIndicator() },
+            contentDescription = null, modifier = Modifier.size(72.dp)
         )
     }
 }
--- app/src/main/java/com/swapnil/profilecard/UserProfile.kt
+++ app/src/main/java/com/swapnil/profilecard/UserProfile.kt
@@ -3,14 +3,38 @@ package com.swapnil.profilecard
 data class UserProfile constructor(
     val userName: String,
     val isActive: Boolean,
-    val drawableId: Int
+    val imageUrl: String
 )
 
 val userProfileList = arrayListOf(
-    UserProfile(userName = "Swapnil Bhojwani", isActive = true, R.drawable.swapnil_profile),
-    UserProfile(userName = "Anna Joans", isActive = false, R.drawable.profile_picture2),
-    UserProfile(userName = "Swapnil Bhojwani", isActive = true, R.drawable.swapnil_profile),
-    UserProfile(userName = "Anna Joans", isActive = false, R.drawable.profile_picture2),
-    UserProfile(userName = "Swapnil Bhojwani", isActive = true, R.drawable.swapnil_profile),
-    UserProfile(userName = "Anna Joans", isActive = false, R.drawable.profile_picture2)
+    UserProfile(
+        userName = "Swapnil Bhojwani",
+        isActive = true,
+        "https://picsum.photos/id/1002/4312/2868"
+    ),
+    UserProfile(
+        userName = "Anna Joans",
+        isActive = false,
+        "https://picsum.photos/id/1011/367/267"
+    ),
+    UserProfile(
+        userName = "Swapnil Bhojwani",
+        isActive = true,
+        "https://picsum.photos/id/1015/6000/4000"
+    ),
+    UserProfile(
+        userName = "Anna Joans",
+        isActive = false,
+        "https://picsum.photos/id/1036/4608/3072"
+    ),
+    UserProfile(
+        userName = "Swapnil Bhojwani",
+        isActive = true,
+        "https://picsum.photos/id/104/3840/2160"
+    ),
+    UserProfile(
+        userName = "Anna Joans",
+        isActive = false,
+        "https://picsum.photos/id/1050/6000/4000"
+    )
 )
