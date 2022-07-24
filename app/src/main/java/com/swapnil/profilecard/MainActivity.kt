aef28122a7c373db0cc2da378dceded8fbc02066
swapnil7000 <swapnil.bhojwani789@gmail.com>
2022-07-24 23:43:58 +0530

    Navigation in compose added

--- app/src/main/java/com/swapnil/profilecard/MainActivity.kt
+++ app/src/main/java/com/swapnil/profilecard/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -18,7 +19,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
@@ -30,32 +36,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ProfileCardTheme {
-                MainScreen()
+                UsersApplication()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ProfileCardTheme {
-        MainScreen()
+        UsersApplication()
     }
 }
 
+@Preview
 @Composable
-fun ProfileCard(userProfile: UserProfile) {
+fun UserProfilePreview() {
+    UserProfileScreen()
+}
+
+@Composable
+fun ProfileCard(userProfile: UserProfile, clickAction: () -> Unit) {
     Card(
         modifier = Modifier
             .padding(16.dp)
             .fillMaxWidth()
-            .wrapContentHeight(Alignment.Top),
+            .wrapContentHeight(Alignment.Top)
+            .clickable {
+                clickAction.invoke()
+            },
         backgroundColor = Color.White,
         elevation = 8.dp
     ) {
@@ -64,19 +74,21 @@ fun ProfileCard(userProfile: UserProfile) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
-            ProfilePicture(userProfile.isActive, userProfile.imageUrl)
-            ProfileContent(userProfile.userName, userProfile.isActive)
+            ProfilePicture(userProfile.isActive, userProfile.imageUrl, 108.dp)
+            ProfileContent(userProfile.userName, userProfile.isActive, Alignment.Start)
         }
     }
 }
 
 @Composable
-fun ProfileContent(userName: String, active: Boolean) {
+fun ProfileContent(userName: String, active: Boolean, alignment: Alignment.Horizontal) {
+
     Column(
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth(),
-        verticalArrangement = Arrangement.SpaceEvenly
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = alignment
     ) {
         Text(text = userName, style = MaterialTheme.typography.h5, color = Color.Black)
         CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
@@ -93,8 +105,11 @@ fun ProfileContent(userName: String, active: Boolean) {
 }
 
 @Composable
-fun ProfilePicture(isActive: Boolean, imageUrl: String) {
+fun ProfilePicture(isActive: Boolean, imageUrl: String, dp: Dp) {
     Card(
+        Modifier
+            .size(dp)
+            .padding(16.dp),
         shape = CircleShape, border = BorderStroke(
             2.dp,
             if (isActive)
@@ -103,23 +118,30 @@ fun ProfilePicture(isActive: Boolean, imageUrl: String) {
                 Color.Red
         )
     ) {
-        SubcomposeAsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(imageUrl)
-                .transformations(CircleCropTransformation()).build(),
-            loading = { CircularProgressIndicator() },
-            contentDescription = null, modifier = Modifier.size(72.dp)
-        )
+        CustomImage(imageUrl = imageUrl)
     }
 }
 
 @Composable
-fun MainScreen(userProfiles: List<UserProfile> = userProfileList) {
+fun CustomImage(imageUrl: String) {
+    SubcomposeAsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            .transformations(CircleCropTransformation()).build(),
+        loading = { CircularProgressIndicator() },
+        contentDescription = null
+    )
+}
+
+@Composable
+fun UsersListScreen(userProfiles: List<UserProfile>, navController: NavHostController) {
     Scaffold(topBar = { AppBar() }) {
         Surface(modifier = Modifier.fillMaxSize()) {
             LazyColumn() {
                 items(userProfiles) { userProfile ->
-                    ProfileCard(userProfile = userProfile)
+                    ProfileCard(userProfile = userProfile) {
+                        navController.navigate("user_detail")
+                    }
                 }
             }
         }
@@ -134,4 +156,42 @@ fun AppBar() {
             Icon(Icons.Default.Home, "Home")
         }
     }, title = { Text(text = "Messaging application") })
+}
+
+
+/*
+* User profile code here*/
+@Composable
+fun UserProfileScreen(userProfile: UserProfile = userProfileList[0]) {
+    Scaffold(topBar = { AppBar() }) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                ProfilePicture(
+                    isActive = userProfile.isActive,
+                    imageUrl = userProfile.imageUrl,
+                    156.dp
+                )
+                ProfileContent(
+                    userName = userProfile.userName,
+                    active = userProfile.isActive,
+                    Alignment.CenterHorizontally
+                )
+            }
+        }
+    }
+}
+
+/*Composable to implement navigation in jetpack
+* */
+@Composable
+fun UsersApplication() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "users_list") {
+        composable("users_list") {
+            UsersListScreen(userProfileList, navController)
+        }
+        composable("user_detail") {
+            UserProfileScreen()
+        }
+    }
 }
--- app/src/main/java/com/swapnil/profilecard/UserProfile.kt
+++ app/src/main/java/com/swapnil/profilecard/UserProfile.kt
@@ -8,33 +8,82 @@ data class UserProfile constructor(
 
 val userProfileList = arrayListOf(
     UserProfile(
-        userName = "Swapnil Bhojwani",
+        userName = "Michaela Runnings",
         isActive = true,
-        "https://picsum.photos/id/1002/4312/2868"
+        "https://images.unsplash.com/photo-1485290334039-a3c69043e517?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
     ),
     UserProfile(
-        userName = "Anna Joans",
+        userName = "John Pestridge",
         isActive = false,
-        "https://picsum.photos/id/1011/367/267"
+        "https://images.unsplash.com/photo-1542178243-bc20204b769f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHBvcnRyYWl0fGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
     ),
     UserProfile(
-        userName = "Swapnil Bhojwani",
+        userName = "Manilla Andrews",
         isActive = true,
-        "https://picsum.photos/id/1015/6000/4000"
+        "https://images.unsplash.com/photo-1543123820-ac4a5f77da38?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDh8fHBvcnRyYWl0fGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
     ),
     UserProfile(
-        userName = "Anna Joans",
+        userName = "Dan Spicer",
         isActive = false,
-        "https://picsum.photos/id/1036/4608/3072"
+        "https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDd8fHBvcnRyYWl0fGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
     ),
     UserProfile(
-        userName = "Swapnil Bhojwani",
+        userName = "Keanu Dester",
+        isActive = false,
+        "https://images.unsplash.com/photo-1597528380214-aa94bde3fc32?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTZ8fHBvcnRyYWl0fGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    ),
+    UserProfile(
+        userName = "Anichu Patel",
+        isActive = false,
+        "https://images.unsplash.com/photo-1598641795816-a84ac9eac40c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjJ8fHBvcnRyYWl0fGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    ),
+    UserProfile(
+        userName = "Kienla Onso",
         isActive = true,
-        "https://picsum.photos/id/104/3840/2160"
+        "https://images.unsplash.com/photo-1566895733044-d2bdda8b6234?ixid=MXwxMjA3fDB8MHxzZWFyY2h8ODh8fHBvcnRyYWl0fGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
     ),
     UserProfile(
-        userName = "Anna Joans",
+        userName = "Andra Matthews",
         isActive = false,
-        "https://picsum.photos/id/1050/6000/4000"
-    )
+        "https://images.unsplash.com/photo-1530577197743-7adf14294584?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTV8fHBvcnRyYWl0fGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    ),
+    UserProfile(
+        userName = "Georgia S.", isActive = false,
+        "https://images.unsplash.com/photo-1547212371-eb5e6a4b590c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTA3fHxwb3J0cmFpdHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    ),
+    UserProfile(
+        userName = "Matt Dengo",
+        isActive = false,
+        "https://images.unsplash.com/photo-1578176603894-57973e38890f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTE0fHxwb3J0cmFpdHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    ),
+    UserProfile(
+        userName = "Marsha T.",
+        isActive = true,
+        "https://images.unsplash.com/photo-1605087880595-8cc6db61f3c6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTI0fHxwb3J0cmFpdHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    ),
+    UserProfile(
+        userName = "Invshu Patel",
+        isActive = true,
+        "https://images.unsplash.com/photo-1561820009-8bef03ebf8e5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTM3fHxwb3J0cmFpdHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    ),
+    UserProfile(
+        userName = "Braylen Nathan",
+        isActive = true,
+        "https://images.unsplash.com/photo-1605857840732-188f2f08cb31?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTQ5fHxwb3J0cmFpdHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    ),
+    UserProfile(
+        userName = "Jorge Andre",
+        isActive = false,
+        "https://images.unsplash.com/photo-1517588632672-9758d6acba04?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTYyfHxwb3J0cmFpdHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    ),
+    UserProfile(
+        userName = "Jamison Dana",
+        isActive = false,
+        "https://images.unsplash.com/photo-1513690277738-c9bc7eb2a992?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTY1fHxwb3J0cmFpdHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    ),
+    UserProfile(
+        userName = "Laura S.",
+        isActive = true,
+        "https://images.unsplash.com/photo-1513708847802-f2fa846a7eaa?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTg1fHxwb3J0cmFpdHxlbnwwfDJ8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    ),
 )
